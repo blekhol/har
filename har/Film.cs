e1064129b351935c3d5aa287210b6408ed2cacaf
Petrik Diak <diak@petrik.hu>
2025-02-07 10:48:49 +0100

    Film class + object

---
+++ har/Film.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace har
+{
+	internal class Film
+	{
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;
+		private string mufaj;
+		private bool megjelent;
+
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = 120;
+			this.mufaj = "Ismeretlen";
+			this.megjelent = false;
+		}
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+		public void Jatszas()
+		{
+            Console.WriteLine("A film elkezdődött");
+        }
+
+		public void MufajModositas(string ujMufaj)
+		{
+			this.mufaj = ujMufaj;
+		}
+
+		public void HosszNovelese(int percek)
+		{
+			this.hosszPercekben += percek;
+		}
+
+		public override string ToString()
+		{
+			return $"Film címe: {this.cim}\nFilm rendezője: {this.rendezo}\nFilm hossza: {this.hosszPercekben}\nFilm műfaja: {this.mufaj}\nMegjelent-e már a film: {this.megjelent}\n";
+		}
+	}
+}
--- har/Program.cs
+++ har/Program.cs
@@ -10,6 +10,7 @@ namespace har
 	{
 		static void Main(string[] args)
 		{
+			//könyv
 			Konyv k = new Konyv("Beszterce ostroma", "Mikszáth Kálmán", 1895, 297, 600);
 			Console.WriteLine(k + "\n");
             k.Arnoveles(800);
@@ -17,6 +18,21 @@ namespace har
 			k.Kedvezmeny(30.0);
 			Console.WriteLine($"Könyv kedvezmjényes ára: {k.Ar}");
 			Console.WriteLine($"Hátramaradt oldalak: {k.HanyOldalMaradt(133)}"); ;
+
+			Console.WriteLine("\n=============");
+
+			//film
+			Film f = new Film("Gázt!", "Nicolas Winding Refn", 100, "Akciófilm", true);
+			Film f2 = new Film("ASD", "harhar");
+			Console.WriteLine(f);
+			Console.WriteLine(f2);
+
+			f.Jatszas();
+			f2.MufajModositas("Akciófilm");
+			Console.WriteLine($"A második film új műfaja: {f2.Mufaj}");
+			f2.HosszNovelese(42);
+			Console.WriteLine($"A második film új műfaja: {f2.HosszPercekben}");
+
 
 			Console.ReadKey();
         }
