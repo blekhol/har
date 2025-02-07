090dac3b8ba71bd0199fd8f91e84bf38fe20af0d
Petrik Diak <diak@petrik.hu>
2025-02-07 10:19:58 +0100

    method 1

--- har/Konyv.cs
+++ har/Konyv.cs
@@ -14,10 +14,37 @@ namespace har
 		private int oldalszam;
 		private int ar;
 
+		public Konyv(string cim, string szerzo, int kiadaseve, int oldalszam, int ar)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			this.kiadaseve = kiadaseve;
+			this.oldalszam = oldalszam;
+			this.ar = ar;
+		}
+
+		public Konyv(string cim, string szerzo)
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+			this.kiadaseve = DateTime.Now.Year;
+			this.oldalszam = 200;
+			this.ar = 3000;
+		}
+
 		public string Cim { get => cim; set => cim = value; }
 		public string Szerzo { get => szerzo; set => szerzo = value; }
 		public int Kiadaseve { get => kiadaseve; set => kiadaseve = value; }
 		public int Oldalszam { get => oldalszam; set => oldalszam = value; }
 		public int Ar { get => ar; set => ar = value; }
+
+		public void Arnoveles(int osszeg)
+		{
+			this.ar = osszeg;
+		}
+
+		
+
+
 	}
 }
